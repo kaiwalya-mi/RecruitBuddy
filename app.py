c07d22ee7d5a282866fe166e8c506e176c1d2ec3
Kaiwalya Mishra <105033838+kaiwalya-mi@users.noreply.github.com>
2025-03-14 18:05:51 -0700

    Add basic project structure

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, redirect, url_for, flash, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev-key-for-testing')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///recruitbuddy.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+# Models will be defined here
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Routes will be defined here
+
+if __name__ == '__main__':
+    app.run(debug=True)
